aa555e7397cfa019f4b67d14058b6a736a2560f5
Dju Makarov <makarovdju@gmail.com>
2024-10-28 19:27:57 +0300

    Completed

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const video = document.querySelector('video')
+
+const swiperText = new Swiper('.swiper', {
+	speed: 1600,
+	mousewheel: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	navigation: {
+		prevEl: '.swiper-button-prev',
+		nextEl: '.swiper-button-next',
+	},
+})
+
+swiperText.on('slideChange', function () {
+	gsap.to(video, 1.6, {
+		currentTime: (video.duration / (this.slides.length - 1)) * this.realIndex,
+		ease: Power2.easeOut,
+	})
+})
+
+swiperText
+	.on('slideChangeTransitionStart', function () {
+		video.classList.add('change')
+	})
+	.on('slideChangeTransitionEnd', function () {
+		video.classList.remove('change')
+	})
